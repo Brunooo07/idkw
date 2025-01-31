88516b4f33c566707cd5559146dfd4c3efaaf992
Bruno Ferreira <134228992+Brunooo07@users.noreply.github.com>
2025-01-31 00:52:15 +0000

    check e fix

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,8 @@
 function Home() {
-   return <h1>
-    Amo ter uma família com você
-   </h1>
-    
+  return <h1>Amo ter uma família com você</h1>;
 }
-export default Home
+export default Home;
+
+function teste2() {
+  console.log("Identação errada");
+}
